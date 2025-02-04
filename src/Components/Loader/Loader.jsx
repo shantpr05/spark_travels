719018a954585cdfebc22d1b83c183133fdc7878
shantpr05 <shanthi.parvatham@hyperisland.se>
2025-02-04 01:40:34 +0100

    comments added and navbar fixed

--- src/Components/Loader/Loader.jsx
+++ src/Components/Loader/Loader.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import styles from './Loader.module.css';
 
+// Loader component displays a loading spinner while data is being fetched or processed.
 const Loader = () => (
   <div className={styles.loaderContainer}>
+    {/* The loader div contains the spinning animation */}
     <div className={styles.loader}></div>
   </div>
 );
